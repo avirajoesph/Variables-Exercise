8ebe870e9c64745e9a9fcae121cc6bba1c166388
Derrick <Avirajoesph@yahoo.com>
2024-05-31 11:47:09 -0500

    intial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int cupcake = 5;
+            char jack = 'j';
+            bool uphill = true;
+            double time = 2.2;
+            decimal feet = 5.5m;
+            string name = "jill";
+
+            Console.WriteLine($"{jack} and {name} walked uphill {feet} feet and it took them {time} minutes" +
+                $" and they bought {cupcake} cupcakes And its {uphill} they made it back downhill ");
         }
     }
 }
